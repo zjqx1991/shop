f241187694d25a772543f02817ebd401b14c6376
RavenWang <zjqx1991@163.com>
2021-07-18 20:49:06 +0800

    乐观锁和分页插件(新版：3.4.0)

--- shop-common/src/main/java/com/lkty/shop/common/config/MybatisPlusConfig.java
+++ shop-common/src/main/java/com/lkty/shop/common/config/MybatisPlusConfig.java
@@ -2,16 +2,23 @@ package com.lkty.shop.common.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MybatisPlusConfig {
+
+    // 注册乐观锁和分页插件(新版：3.4.0)
     @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor()); // 乐观锁插件
+        // DbType：数据库类型(根据类型获取应使用的分页方言)
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL)); // 分页插件
         return interceptor;
     }
+
 }
